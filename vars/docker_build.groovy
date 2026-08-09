8302cc88dc74d7d78671715d6b7ee3e4e19f838c
cshubham1727 <134256211+cshubham1727@users.noreply.github.com>
2026-08-10 00:12:15 +0530

    Update Docker build function to include username

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,12 +1,16 @@
 def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def dockerfile = config.dockerfile ?: 'Dockerfile'
-    def context = config.context ?: '.'
-    
-    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
-    
+    def rawImageName = config.imageName ?: error("Image name is required")
+    def imageTag     = config.imageTag ?: 'latest'
+    def dockerfile   = config.dockerfile ?: 'Dockerfile'
+    def context      = config.context ?: '.'
+    def username     = config.username ? "${config.username}/" : ""
+
+    // Combines username + imageName (e.g., cshubham1727/two-tier-flask-app)
+    def fullImageName = "${username}${rawImageName}"
+
+    echo "Building Docker image: ${fullImageName}:${imageTag} using ${dockerfile}"
+
     sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+        docker build -t ${fullImageName}:${imageTag} -t ${fullImageName}:latest -f ${dockerfile} ${context}
     """
 }
