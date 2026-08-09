08afb3e41d9704ed545343a6e6fb4ea05e9155a4
cshubham1727 <134256211+cshubham1727@users.noreply.github.com>
2026-08-10 00:12:41 +0530

    Refactor docker_push to use full image name

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,19 +1,23 @@
 def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def credentials = config.credentials ?: 'dockerHubCreds'
-    
-    echo "Pushing Docker image: ${imageName}:${imageTag}"
-    
+    def rawImageName = config.imageName ?: error("Image name is required")
+    def imageTag     = config.imageTag ?: 'latest'
+    def credentials  = config.credentials ?: 'dockerHubCreds'
+
     withCredentials([usernamePassword(
         credentialsId: credentials,
         usernameVariable: 'DOCKER_USERNAME',
         passwordVariable: 'DOCKER_PASSWORD'
     )]) {
+        // If config.username is provided, use it; otherwise fallback to $DOCKER_USERNAME from credentials
+        def userPrefix = config.username ?: "\$DOCKER_USERNAME"
+        def fullImageName = "${userPrefix}/${rawImageName}"
+
+        echo "Pushing Docker image: ${fullImageName}:${imageTag}"
+
         sh """
             echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
-            docker push ${imageName}:${imageTag}
-            docker push ${imageName}:latest
+            docker push ${fullImageName}:${imageTag}
+            docker push ${fullImageName}:latest
         """
     }
 }
